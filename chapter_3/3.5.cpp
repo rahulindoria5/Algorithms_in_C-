7767564833a6d17f8bdf9f312a99b86bf69cfc5d
rahulindoria5 <rahulindoria2006@gmail.com>
2022-04-21 01:04:56 +0530

    3.5.cpp updated

--- chapter_3/3.5.cpp
+++ chapter_3/3.5.cpp
@@ -1,12 +1,19 @@
 // generate random binary bits
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main()
 {
-  for (int i = 0; i < 10; i++)
+  srand(time(0));  // Initialize random number generator.
+  int num_of_rand; // enter r = 2, 4 and 16
+
+  cout << "how many random numbers you want to generate ";
+  cin >> num_of_rand;
+
+  for (int i = 0; i < num_of_rand; i++)
   {
-    cout << "random bits is : " << rand()%2 << endl;
+    cout << "random number " << i << " is : " << rand()%2 << endl;
   }
 
   return 0;
