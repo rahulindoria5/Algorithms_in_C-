9aa292db0d3232c986b37e33f17a272fa5a1331c
rahulindoria5 <rahulindoria2006@gmail.com>
2022-04-21 18:10:24 +0530

    code updated

--- chapter_3/examples/program_3.5.cpp
+++ chapter_3/examples/program_3.5.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int * find_prime_numbers(int N, int a[])
+{
+  // finding prime numbers
+  for (int i = 2; i <=N; i++)
+  {
+    if(a[i])
+    {
+      for(int j = i; j*i <= N; j++)
+      {
+        a[i*j] = 0;
+      }
+    }
+  }
+  return a;
+}
+
+void display_prime_numbers(int *k, int N)
+{
+  // display prime numbers
+  for(int i = 2; i <= N; i++)
+    cout << "I is " << i << " : " << *(k + i) << endl;
+}
+
 int main()
 {
   int N;
@@ -11,23 +34,9 @@ int main()
   for (int i = 2; i <= N; i++)
     a[i] = 1;
 
-  // for (int i = 1; i <= N; i++)
-  //   cout << "I is : " << i << " : " << a[i] << endl;
-
-  for (int i = 2; i <=N; i++)
-  {
-    if(a[i])
-    {
-      for(int j = i; j*i <= N; j++)
-      {
-
-        a[i*j] = 0;
-      }
-    }
-  }
-
-  for(int i = 2; i <= N; i++)
-    cout << "I is " << i << " : " << a[i] << endl;
+  int *k;
+  k = find_prime_numbers(N, a);
+  display_prime_numbers(k, N);
 
   return 0;
 }
